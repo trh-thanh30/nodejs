1011fb3f70b26fe13a88419349a362254c09932b
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-25 23:24:38 +0700

    commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
+const chalk = require("chalk");
 const validator = require("validator");
 const { name, add } = require("./utils.js");
 const { sum } = require("./sum.js");
 console.log(name);
 console.log(add(2, 3));
-console.log(sum(3, 3));
-console.log(validator.isEmail("thanh@gmail.com"));
+console.log(chalk.red(sum(3, 3)));
+console.log(chalk.white.bgGreen(validator.isEmail("thanh@gmail.com")));
+console.log(chalk.green("Success!"));
