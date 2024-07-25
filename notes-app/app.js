35770176382466de4eeca97c539c561a46e4fd5c
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-25 22:57:34 +0700

    importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
+const validator = require("validator");
 const { name, add } = require("./utils.js");
 const { sum } = require("./sum.js");
 console.log(name);
 console.log(add(2, 3));
 console.log(sum(3, 3));
+console.log(validator.isEmail("thanh@gmail.com"));
