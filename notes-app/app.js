6e99834bf0742f17922e220634b03d1ef540f45a
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-26 17:23:25 +0700

    dcommit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,52 @@
 const chalk = require("chalk");
-const validator = require("validator");
-const { name, add } = require("./utils.js");
-const { sum } = require("./sum.js");
-console.log(name);
-console.log(add(2, 3));
-console.log(chalk.red(sum(3, 3)));
-console.log(chalk.white.bgGreen(validator.isEmail("thanh@gmail.com")));
-console.log(chalk.green("Success!"));
+const yargs = require("yargs");
+// Customize yargs version
+yargs.version("1.1.0");
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    decreption: {
+      describe: "Note description",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Title: " + argv.title);
+    console.log("Decreption: " + argv.decreption);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing the note");
+  },
+});
+// Created read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading the note");
+  },
+});
+// Created list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: () => {
+    console.log("Listing all notes");
+  },
+});
+// add, remove, read, list
+yargs.parse();
