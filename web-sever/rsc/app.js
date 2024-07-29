4242b2a7a1b265e238a48822112a8fa88904c688
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-29 09:30:54 +0700

    hello express

---
+++ web-sever/rsc/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/help", (req, res) => {
+  res.send("Help Page");
+});
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+app.get("/weather", (req, res) => {
+  res.send("Weather Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
