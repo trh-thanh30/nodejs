7db8cb6dc27731fc344977a81b1e75d154a9df6e
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-29 16:16:54 +0700

    web-sever

--- web-sever/rsc/app.js
+++ web-sever/rsc/app.js
@@ -1,19 +1,60 @@
+const path = require("path");
 const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const hbs = require("hbs");
 
-app.get("/help", (req, res) => {
-  res.send("Help Page");
+// Define paths for express config
+const app = express();
+const port = 3000;
+
+// Setup handlebars engine and views location
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+// Setup static directory to serve
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Huu Thanh",
+    desc: " Use this site to your weather!",
+  });
 });
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.render("about", {
+    title: "About Page",
+    name: "Huu Thanh",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Huu Thanh",
+    desc: "Help section for Huu Thanh",
+  });
 });
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({
+    location: "Hanoi",
+    temperature: 25,
+    weather_descriptions: "sunny",
+  });
+});
+app.get("/help/*", (req, res) => {
+  res.render("not-found", {
+    errorMess: "Help article not found",
+    name: "Huu Thanh",
+  });
+});
+app.get("*", (req, res) => {
+  res.render("not-found", {
+    errorMess: "Page not found",
+    name: "Huu Thanh",
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
